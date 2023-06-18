25dacfca3d979092a667a9a7e77941bcf73ed0a1
Crowdbotics <team@crowdbotics.com>
2023-06-18 00:09:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-smoke-41566",
+  url: "https://curly-smoke-41566.botics.co",
+  api: "https://curly-smoke-41566.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
